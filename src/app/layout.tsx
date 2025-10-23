6d887c7b03c6113c38c962abe1073f5ba6273acd
RynoIsJeff <vdwryno@gmail.com>
2025-10-23 19:58:31 +0100

    Fixed accidental removal of imports in app>layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,8 @@
-// src/app/layout.tsx
+import "./globals.css"
+import type { Metadata } from "next"
+
+export const metadata: Metadata = { title: "SupaTrade Group" }
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html
